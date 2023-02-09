8e3dec4918535f117758e2d6f8e3afda8baf9ed0
David Johnson <dmjohnson844@gmail.com>
2023-02-09 01:55:09 -0500

    added Jquery to slideToggle directors text//tweaking elements to ensure pages are fully responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+       $(".plus-sign")
+        .on("click", function() {
+          $(".about-hidden-logo-container").slideToggle();  
+          $(".about-hidden-p").slideToggle(); 
+
+        })
+  })
+</script>
+
